deada0f864e16e1109dc141bfa232f4f03745799
liguoq4 <guoqing.li@intel.com>
2020-10-19 12:49:14 -0700

    updating

--- aliases.sh
+++ aliases.sh
@@ -1,6 +1,6 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 export PETERINO_DOTFILES_DIR=${DIR}
-alias update_dotfiles="cd $(echo ${PETERINO_DOTFILES_DIR}) && git commit -am 'updating' && git pull"
+alias update_dotfiles="cd $(echo ${PETERINO_DOTFILES_DIR}) && git commit -am 'updating' && git pull && git push"
 export PATH="$PATH:${DIR}/scripts"
 echo Loading Peterino\'s Dotfiles at  \>\>\> ${DIR}
 
