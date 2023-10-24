6cbcc79d673afcf8ad28376377d8515bd3db95ce
Peter Li <lipet@microsoft.com>
2023-10-24 10:29:12 -0700

    removed neovide nonsense

--- install.py
+++ install.py
@@ -1,9 +1,8 @@
-import os 
-import pathlib 
+import os
+import pathlib
 import subprocess
 import shutil
 import argparse
-
 
 orig_dir = os.path.dirname(os.path.abspath(__file__))
 repo_dir = orig_dir
@@ -21,14 +20,14 @@ def install_dependencies(args):
         os.system('powershell -Command "Set-ExecutionPolicy RemoteSigned -scope CurrentUser"')
         os.system('powershell -Command "Set-ExecutionPolicy RemoteSigned -scope CurrentUser"')
         os.system('powershell -Command "Invoke-Expression (New-Object System.Net.WebClient).DownloadString(\'https://get.scoop.sh\')')
-        
+
         os.system('scoop install fzf')
         os.system('scoop install ripgrep')
         os.system('scoop install ctags')
         os.system('scoop bucket add extras')
         os.system('scoop install neovim')
         os.system('setx FZF_DEFAULT_COMMAND "rg --files --no-ignore-vcs --hidden"')
-    else: 
+    else:
         if not args.skip_packages:
             if args.manjaro:
                 os.system('yay -S fzf')
@@ -42,14 +41,13 @@ def install_dependencies(args):
                 os.system('snap install neovim')
 
             should_set_vars = input("I want to set the Variable FZF_DEFAULT_COMMAND to 'rg --files --no-ignore-vcs --hidden' by appending it to your ~/.profile. do you want this? [Y/n]")
-            if "y" in should_set_vars.lower(): 
+            if "y" in should_set_vars.lower():
                 os.system('echo export FZF_DEFAULT_COMMAND="rg --files --no-ignore-vcs --hidden" >> ~/.profile')
                 print('done')
             else:
                 print('not done')
 
-def setup_neovide(args):
-
+def setup_neovim(args):
     os.makedirs(colors_path, exist_ok=True)
     os.makedirs(config_path, exist_ok=True)
 
@@ -85,34 +83,12 @@ def setup_neovide(args):
 
         vim_user_path = os.path.join(user_path, '.vim/')
 
-def setup_neovide_deps():
-    if os.name == 'nt':
-        os.system('scoop uninstall neovide')
-        os.system('scoop install cmake')
-        os.system('scoop install llvm')
-
-def setup_neovide_dev(args):
-    setup_neovide_deps()
-    if not os.path.exists('~/neovide'):
-        os.system(f'git clone https://github.com/neovide/neovide {os.path.join(user_path, "neovide")}')
-    else:
-        os.system('cd ~/neovide/ && git fetch && git pull && ' + \
-                'git checkout -f')
-    my_apps_path = os.path.abspath(os.path.join(user_path, 'my_apps'))
-    os.makedirs(my_apps_path, exist_ok = True)
-    os.system(f'cd {os.path.join(user_path, "neovide")} && cargo build --release')
-    shutil.copy(os.path.join(user_path, 'neovide/target/release/neovide.exe'), f'{my_apps_path}')
-    if os.name == 'nt':
-        os.system('setx NEOVIDE_MULTIGRID "true"')
-        print("Add ~/my_apps to PATH and you're good to go")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-
     print(config_path)
-
     parser.add_argument("--manjaro", help="executes the installer scripts for manjaro", action='store_true')
     parser.add_argument("--skip-packages", help="skips trying to install packages", action='store_true')
     args = parser.parse_args()
     install_dependencies(args)
-    setup_neovide(args)
+    setup_neovim(args)
